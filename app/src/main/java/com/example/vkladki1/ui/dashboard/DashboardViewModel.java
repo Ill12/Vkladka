c5769af52e5758f36f8393977a565de46b84e214
Ill12 <103013164+Ill12@users.noreply.github.com>
2024-02-15 13:46:06 +0900

    commit

--- app/src/main/java/com/example/vkladki1/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/vkladki1/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Желтый букет");
+        mText.setValue("Красный букет");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/example/vkladki1/ui/osebe/osebeViewModel.java
+++ app/src/main/java/com/example/vkladki1/ui/osebe/osebeViewModel.java
@@ -1,16 +1,16 @@
-package com.example.vkladki1.ui.home;
+package com.example.vkladki1.ui.osebe;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class osebeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public osebeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Желтый букет");
+        mText.setValue("Илларионов Илларион");
     }
 
     public LiveData<String> getText() {
